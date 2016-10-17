bb7ef8f2c072b2a6fb71ea36b5cce314b3e2771b
mitzimagination <mitzimagination@gmail.com>
2016-10-17 09:51:50 -0700

    404 page and about page additions

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -36,7 +36,25 @@
             'rewrite' => array( 'slug' => 'case-studies' ),
         )
     );
+    register_post_type( 'services',
+        array(
+            'labels' => array(
+                'name' => __( 'Services' ),
+                'singular_name' => __( 'Service' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array( 'slug' => 'services' ),
+        )
+    );
 }
+
+
 add_action( 'init', 'create_custom_post_types' );
+
+add_theme_support( 'post-thumbnails' );
+
+
+
 
 ?>
--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -13,32 +13,64 @@
  */
 
 get_header(); ?>
+
 <section class="about-page">
 
 	<div class="site-content">
 		<?php while ( have_posts() ) : the_post(); ?>
 			<div class='about-hero'>
 				<?php the_content(); ?>
-				<h1>Accelerate is a strategy and design marketing
-                agency located in the heart of NYC. Our goal is to 
-                build businesses by making our clients visible and
-                their customers smile.</h1></div>
+				<h1><span class="color-scheme">Accelerate</span> is a marketing and strategy company located
+                in the heart of NYC.  We build business by making our clients
+                visible and their customers smile.</h1>
+			</div>
 		<?php endwhile; // end of the loop. ?>
-	</div><!-- .container -->
+	</div><!-- .site-content -->
 </section><!-- .about-page -->
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
+
+<section class="about-page-services-title">
+
+    <div class="services-title">
+    
+        <h3>OUR SERVICES</h3>
+        <p>We take pride in our clients and the content we create
+        for them.<br>Here's a brief overview of our offered services.</p>
+
+    </div><!--.site_content-->
+
+
+</section><!--.about_page-->
+
+<section class="services">
+    <div class="site-content">
+     
+       <?php query_posts('posts_per_page=4&post_type=services'); ?>
+                 
+       <?php while ( have_posts() ) : the_post(); 
+                
+                $image_1 = get_field("image_1");
+                $size = "medium";?>
+
+        <div class ="about-services-posts">
+        <div class ="about-services-images">
+
+        <?php echo wp_get_attachment_image($image_1, $size); ?></div>
+
+        <div class ="about-services-content">
+        <div class ="services-post-title"><?php the_title(); ?></div>
+
+        <?php the_content(); ?></div>
+        
+            </div>
+
+            </div>
+                
            
+        <?php endwhile; //end of the loop. ?>
+        <?php wp_reset_query(); // resets the altered query back to the original ?>
 
-            <?php the_content(); ?>
-			
+</section>
 
-				
-			<?php endwhile; // end of the loop. ?>
-
-		</div><!-- #content -->
-	</div><!-- #primary -->
 
 
 <?php get_footer(); ?>
